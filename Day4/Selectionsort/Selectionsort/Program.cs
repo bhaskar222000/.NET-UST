0c186381fa0c060bde03aefbef15fcfa7ff71eab
bhaskar222000 <129932164+bhaskar222000@users.noreply.github.com>
2023-04-14 09:12:37 +0530

    Add files via upload

---
+++ Day4/Selectionsort/Selectionsort/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Selectionsort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number of values:");
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+            selectsort(arr);
+        }
+        public static void selectsort(int[] arr)
+        {
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                int a = i;
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[j] < arr[a])
+                    {
+                        a = j;
+                    }
+                    int temp = arr[a];
+                    arr[a] = arr[i];
+                    arr[i] = temp;
+                }
+            }
+            foreach (int i in arr)
+            {
+                Console.Write(i + " ");
+            }
+        }
+    }
+}
---
+++ Day4/Stringexample/Stringexample/Program.cs
@@ -0,0 +1,21 @@
+﻿//simple string example
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stringexample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s;
+            Console.Write("Enter string value :");
+            s= Console.ReadLine();
+            Console.WriteLine(s.ToUpper());
+            Console.WriteLine(s.ToLower()); 
+        }
+    }
+}

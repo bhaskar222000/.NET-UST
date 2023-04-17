1973171f089b5ee3ea0d841ec2161c02131171ff
bhaskar222000 <129932164+bhaskar222000@users.noreply.github.com>
2023-04-17 17:31:12 +0530

    Add files via upload

---
+++ Day9/AssignmentMail/AssignmentMail/Mail.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentMail
+{
+    public class Mail
+    {
+        private long _id;
+        public long ID
+        {
+            get {
+                return _id;
+            }
+            set
+            {
+                _id = value;
+            }
+                }
+        private string _to;
+        public string TO
+        {
+            get
+            { 
+                return _to;
+            }
+            set
+            {
+                _to = value;
+            }
+        }
+        private string _from;
+        public string FROM
+        {
+            get
+            {
+                return _from;
+            }
+            set { _from = value; }
+        }
+        private string _subject;
+        public string SUBJECT
+        {
+            get
+            {
+                return _subject;
+            }
+            set
+            {
+                _subject = value;
+            }
+        }
+        private string _content;
+        public string CONTENT
+        { get {
+                return _content;
+            }
+            set
+            {
+                _content= value;
+            }
+        }
+        private DateTime _receivedDate;
+        public DateTime RECEIVEDDATE
+        {
+            get
+            {
+                return _receivedDate;   
+            }
+            set
+            {
+                _receivedDate = value;
+            }
+        }
+        private double _size;
+        public double SIZE
+        {
+            get
+            {
+                return _size;
+            }
+            set
+            {
+                _size = value;
+            }
+        }
+
+        public Mail()
+        {
+
+        }
+        public Mail(long _id, string _to, string _from, string _subject, string _content, DateTime _receivedDate, double _size)
+        {
+            this._id = _id;
+            this._to = _to;
+            this._from = _from;
+            this._subject = _subject;
+            this._content = _content;
+            this._receivedDate = _receivedDate;
+            this._size = _size;
+
+        }
+        public override string ToString()
+        {
+            return string.Format("_id:{0}\n_to:{1}\n_from{2}\n_subject{3}\n_content{4}\n_receiceddDate{5}\n_size{6}\n",
+                               _id,_to,_from,_subject,_content,_receivedDate.ToString("dd-MM-yyyy", null),_size.ToString("0.0"));
+        }
+        public override bool Equals(object obj)
+        {
+            Mail m = obj as Mail;
+            if (m.TO == TO && m.FROM == FROM && m.SUBJECT == SUBJECT)
+                return true;
+            else 
+                return false;   
+        }
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+    }
+
+}
---
+++ Day9/AssignmentMail/AssignmentMail/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentMail
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+            Console.WriteLine("Enter Mail1 details: ");
+           
+            string[] s = Console.ReadLine().Split(',');
+            Mail m1 = new Mail(int.Parse(s[0]), s[1], s[2], s[3], s[4],
+                Convert.ToDateTime(s[5]), Convert.ToDouble(s[6]));
+            Console.WriteLine("Enter Mail2 details: ");
+
+            string[] s2 = Console.ReadLine().Split(',');
+            Mail m2 = new Mail(int.Parse(s2[0]), s2[1], s2[2], s2[3], s2[4],
+                Convert.ToDateTime(s2[5]), Convert.ToDouble(s2[6]));
+            
+            Console.WriteLine("Mail 1 :\n" + m1);
+            Console.WriteLine("Mail 2 : \n" + m2);
+
+            if (s[1] == s2[1] && s[2] == s2[2] && s[3] == s2[3])
+            {
+                Console.WriteLine("Mail 1 is same as Mail2");
+            }
+            else
+            {
+                Console.WriteLine("Mail 1 and Mail 2 is different");
+            }
+        }
+    }
+}

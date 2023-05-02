b4667bce6f73876e7eac8f154500bc39377ac888
bhaskar222000 <129932164+bhaskar222000@users.noreply.github.com>
2023-05-02 16:56:40 +0530

    Add files via upload

---
+++ Day21/NUnit/NUnit/EmployeeDetails.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnit
+{
+    public class EmployeeDetails
+    {
+        public int id
+        {
+            get;
+            set;
+        }
+        public string Name
+        {
+            get;
+            set;
+        }
+        public double salary
+        {
+            get;
+            set;
+        }
+        public string Gender
+        {
+            get;
+            set;
+        }
+    }
+}
+

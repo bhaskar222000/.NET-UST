0563b16e3cdc03935dc7988ab8b017d2f66a2214
bhaskar222000 <129932164+bhaskar222000@users.noreply.github.com>
2023-06-08 17:23:20 +0530

    Add files via upload

---
+++ Day44/Bhaskar/BigIntegerPrac/BigIntegerPrac/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace BigIntegerPrac
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            BigInteger a;
+
+            BigInteger b;
+
+            Console.WriteLine("Enter number 1:");
+
+            a = BigInteger.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter number2 :");
+
+            b = BigInteger.Parse(Console.ReadLine());
+
+            BigInteger sum = a + b;
+
+            BigInteger prod = a * b;
+
+            Console.WriteLine(sum);
+
+            Console.WriteLine(prod);
+
+        }
+
+    }
+}
+   

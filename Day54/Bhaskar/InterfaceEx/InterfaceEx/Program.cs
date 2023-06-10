4be4f34eac79a1d28de6529e77a50d9eaeb17647
bhaskar222000 <129932164+bhaskar222000@users.noreply.github.com>
2023-06-10 10:15:17 +0530

    Add files via upload

---
+++ Day54/Bhaskar/InterfaceEx/InterfaceEx/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceEx
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            AdvancedArithmetic myCal = new MyCalculator();
+            int sum = myCal.divisorSum(n);
+            Console.WriteLine("I implemented: AdvancedArithmetic");
+            Console.WriteLine(sum);
+        }
+    }
+}
